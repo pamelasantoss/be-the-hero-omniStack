5680e82ea85a74feb7f30e97457727fdcf766cba
Pamela <pamesantoss@hotmail.com>
2020-03-25 19:30:23 -0300

    Criando página de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Header from './Header';
+import Logon from './pages/Logon';
+
+import './global.css';
 
 function App() {
   return (
-    <Header title="Semana OmniStack" />
+    <Logon />
   );
 }
 
