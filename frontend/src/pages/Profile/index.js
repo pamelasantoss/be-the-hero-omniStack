80414d53b8d0f59e233f823b3c2ab8c72cf9dcb5
Pamela <pamesantoss@hotmail.com>
2020-03-25 22:09:01 -0300

    Criando função para deslogar da página

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -10,6 +10,8 @@ import logoImg from '../../assets/logo.svg';
 
 function Profile() {
   const [incidents, setIncidents] = useState([])
+
+  const history = useHistory();
 
   const ongId = localStorage.getItem('ongId')
   const ongName = localStorage.getItem('ongName')
@@ -38,6 +40,12 @@ function Profile() {
     }
   }
 
+  function handleLogout() {
+    localStorage.clear()
+
+    history.push('/')
+  }
+
   return (
     <div className="profile-container">
       <header>
@@ -46,7 +54,7 @@ function Profile() {
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
 
-        <button type="button">
+        <button type="button" onClick={handleLogout}>
           <FiPower size={18} color="#E02041" />
         </button>
       </header>
