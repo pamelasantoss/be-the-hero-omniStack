a16257c60da9cb5f44a7cd1bddf016e7b9526356
Pamela <pamesantoss@hotmail.com>
2020-03-25 21:46:29 -0300

    Trazendo nome da ONG na pág de perfil

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -7,11 +7,13 @@ import './styles.css';
 import logoImg from '../../assets/logo.svg';
 
 function Profile() {
+  const ongName = localStorage.getItem('ongName')
+
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="Be The Hero" />
-        <span>Bem Vinda, APAD</span>
+        <span>Bem Vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
 
